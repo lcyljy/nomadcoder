2fc3ea20faed09144dc500c1f4a9328c81e6f8cf
lcyljy <35355087+lcyljy@users.noreply.github.com>
2022-03-19 00:22:20 +0900

    1

---
+++ VanillajsChrome/momentum/app.js
@@ -0,0 +1,23 @@
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  multi: function (a, b) {
+    console.log(a * b);
+  },
+  div: function (a, b) {
+    console.log(a / b);
+  },
+  powerOf: function (a, b) {
+    console.log(a ** b);
+  },
+};
+
+calculator.add(4, 5);
+calculator.minus(4, 5);
+calculator.multi(4, 5);
+calculator.div(4, 5);
+calculator.powerOf(4, 5);
