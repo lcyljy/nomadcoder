105dfcfe2f880800db44a84b9a88a30be763985b
lcyljy <35355087+lcyljy@users.noreply.github.com>
2022-03-23 00:32:23 +0900

    #5.3까지 학습

--- VanillajsChrome/momentum/app.js
+++ VanillajsChrome/momentum/app.js
@@ -3,11 +3,13 @@ const loginButton = document.querySelector("#login-form button");
 
 
 function onLoginBtnClick() {
-  const value = loginInput.value;
-  if (value === "") {
+  const username = loginInput.value;
+  if (username === "") {
     alert("Please write your name");
+  } else if (username.length > 15) {
+    alert("Your name is too long.")
   } else {
-    console.log("click!!!", value);
+    console.log("click!!!", username);
   }
 
 }
