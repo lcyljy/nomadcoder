69b782275ccf5092ff02a64eda64058960bf8921
lcyljy <35355087+lcyljy@users.noreply.github.com>
2022-04-24 12:11:48 +0900

    Create todo.js

---
+++ VanillajsChrome/Challenge/src/js/todo.js
@@ -0,0 +1,60 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = document.querySelector("#todo-form input")
+const toDoList = document.getElementById("todo-List")
+
+const TODOS_KEY = "todos"
+
+let toDos = [];
+
+function saveToDos() {
+  localStorage.setItem("TODOS_KEY", JSON.stringify(toDos))
+}
+
+function deleteToDo(e) {
+  const li = e.target.parentElement;
+  li.remove();
+  toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id))
+  saveToDos();
+}
+
+function paintToDo(newToDo) {
+  const li = document.createElement("li");
+  li.id = newToDo.id
+  const span = document.createElement("span");
+  span.textContent = newToDo.text;
+  const button = document.createElement("button");
+  button.textContent = "X"
+  button.addEventListener("click", deleteToDo)
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li)
+}
+
+function handleToDoSubmit(e) {
+  e.preventDefault();
+  const newToDo = toDoInput.value;
+  toDoInput.value = "";
+  const newTodoObj = {
+    text: newToDo,
+    id: Date.now()
+  }
+  toDos.push(newTodoObj)
+  paintToDo(newTodoObj)
+  saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit)
+
+
+
+const savedToDos = localStorage.getItem("TODOS_KEY")
+
+console.log(savedToDos)
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo)
+
+}
+
